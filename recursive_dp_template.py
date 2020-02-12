ec9076657ea196a606653f539d7dfd06a9bdb04b
L28-1-n-1-n <zto2.71828@gmail.com>
2020-02-12 00:45:21 +0000

    fixing error with regards to sequences of different lengths

--- recursive_dp_template.py
+++ recursive_dp_template.py
@@ -14,19 +14,19 @@ import sys
 # DO NOT EDIT ------------------------------------------------
 # Given an alignment, which is two strings, display it
 #
-# def displayAlignment(alignment):
-#     string1 = alignment[0]
-#     string2 = alignment[1]
-#     string3 = ''
-#     for i in range(min(len(string1),len(string2))):
-#         if string1[i]==string2[i]:
-#             string3=string3+"|"
-#         else:
-#             string3=string3+" "
-#     print('Alignment ')
-#     print('String1: '+string1)
-#     print('         '+string3)
-#     print('String2: '+string2+'\n\n')
+def displayAlignment(alignment):
+    string1 = alignment[0]
+    string2 = alignment[1]
+    string3 = ''
+    for i in range(min(len(string1),len(string2))):
+        if string1[i]==string2[i]:
+            string3=string3+"|"
+        else:
+            string3=string3+" "
+    print('Alignment ')
+    print('String1: '+string1)
+    print('         '+string3)
+    print('String2: '+string2+'\n\n')
 
 # ------------------------------------------------------------
 
@@ -48,12 +48,51 @@ start = time.time()
 # The sequences are contained in the variables seq1 and seq2 from the code above.
 # To work with the printing functions below the best local alignment should be called best_alignment and its score should be called best_score. 
 
+def result_str(matrix, best_alignment, seq1, seq2):
+    i = len(seq2)
+    j = len(seq1)
+    str0 = ""
+    str1 = ""
+    while ((i != 0) and (j != 0)):
+        if (matrix[i][j] == "D"):
+
+            # best_alignment[1] += str(seq2[i - 1])
+            # best_alignment[0].append(seq1[j - 1])
+
+            str1 += str(seq2[i - 1])
+            str0 += str(seq1[j - 1])
+
+            i -= 1
+            j -= 1
+
+        elif (matrix[i][j] == "U"):
+
+            # best_alignment[1].append(seq2[i - 1])
+            # best_alignment[0].append("-")
+
+            str1 += str(seq2[i - 1])
+            str0 += "-"
+            i -= 1
+        else:
+
+            # best_alignment[1].append("-")
+            # best_alignment[0].append(seq1[j - 1])
+
+            str1 += "-"
+            str0 += str(seq1[j - 1])
+
+            j -= 1
+    best_alignment[1] = str1[::-1]
+    best_alignment[0] = str0[::-1]
+    print(best_alignment)
+    return(best_alignment)
+
+
 def calc(seq1, seq2, i, j):
-    # if (i == (len(seq1) + 1)) or (j == (len(seq1) + 1)):
-    #     return (arr)
     # print(i)
     # print(j)
     up = seq2[i - 1]
+    print(" i is " + str(i) + "and j is " + str(j))
     left = seq1[j - 1]
 
     if (up == left):
@@ -67,13 +106,6 @@ def calc(seq1, seq2, i, j):
             match = 2
     else:
         match = -3
-    # top = arr[i - 1][j] - 4
-    # left = arr[i][j - 1] - 4
-    # diagonal = arr[i - 1][j - 1] + match
-    # arr[i][j] = max(top, left, diagonal);
-    # print("match is " + str(match))
-    #
-    # return (arr[i + 1][j + 1])
     return (match)
 
 def recursion(seq1, seq2, i, j, arr, matrix):
@@ -95,28 +127,31 @@ def recursion(seq1, seq2, i, j, arr, matrix):
         dict.update({"D" : calc(seq1, seq2, i, j) + recursion(seq1, seq2, i - 1, j - 1, arr, matrix)})
         dict.update({"U": recursion(seq1, seq2, i - 1, j, arr, matrix) - 4})
         dict.update({"L": recursion(seq1, seq2, i, j - 1, arr, matrix) - 4})
-        # if (i == 2) and (j == 3):
-        #     print(dict)
-        #     print(calc(seq1, seq2, i, j))
+
         arr[i][j] = max(dict.values())
         matrix[i][j] = list(dict.keys())[list(dict.values()).index(arr[i][j])]
         # return(max(calc(seq1, seq2, i, j) + arr[i - 1][j - 1], arr[i - 1][j] - 4, arr[i][j - 1] - 4))
         # arr[i][j] = max(calc(seq1, seq2, i, j) + recursion(seq1, seq2, i - 1, j - 1, arr), recursion(seq1, seq2, i - 1, j, arr) - 4, recursion(seq1, seq2, i, j - 1, arr) - 4)
         # return (max(calc(seq1, seq2, i, j) + recursion(seq1, seq2, i - 1,j - 1, arr, matrix), recursion(seq1, seq2, i - 1, j, arr, matrix) - 4, recursion(seq1, seq2, i, j - 1, arr, matrix) - 4))
         return(max(dict.values()))
+
 def compose(seq1, seq2):
     print(seq1)
     print(seq2)
     matrix = [matrix[:] for matrix in [['*'] * (len(seq1) + 1)] * (len(seq2) + 1)]
     arr = [arr[:] for arr in [['*'] * (len(seq1) + 1)] * (len(seq2) + 1)]
+    print(matrix)
     # for i in range(0, len(seq1)):
     #     arr[0][i] = -1 * i
     # for j in range(0, len(seq2)):
     #     arr[j][0] = -1 * j
     # print(calc(arr, 1, 1))
-    print (recursion(seq1, seq2, len(seq1), len(seq2), arr, matrix))
+    print (recursion(seq1, seq2, len(seq2), len(seq1), arr, matrix))
     print(arr)
     print(matrix)
+    best_alignment = [best_alignment[:] for best_alignment in [[] * (max(len(seq1), len(seq2)) + 1)] * 2]
+    print(best_alignment)
+    displayAlignment(result_str(matrix, best_alignment, seq1, seq2))
 
 compose(seq1, seq2)
 #-------------------------------------------------------------
